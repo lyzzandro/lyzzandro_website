4d3d3931307390baab6e3eece2cd93ec52edd473
Lyzzandro <lyzzandro91@gmail.com>
2023-06-14 08:41:30 -0300

    first commit

---
+++ src/components/menu/index.js
@@ -0,0 +1,23 @@
+import '../../styles/Menu.css';
+import logo from '../../images/logo.svg';
+export default function Menu() {
+    return (
+        <header className="Menu">
+            <img src={logo} />
+            <ul>
+                <li>
+                    <a>Sobre mim</a>
+                </li>
+                <li>
+                    <a>Sobre mim</a>
+                </li>
+                <li>
+                    <a>Sobre mim</a>
+                </li>
+                <li>
+                    <a>Sobre mim</a>
+                </li>
+            </ul>
+        </header>
+    );
+}
