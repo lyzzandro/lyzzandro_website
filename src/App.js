e66c986e5746c9a93b80480e43cf21699cf1083d
Lyzzandro <lyzzandro91@gmail.com>
2023-06-14 18:02:45 -0300

    add menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/menu';
+import Apresentation from './components/apresentation';
 
 function App() {
     return (
         <div className="App">
             <Menu />
+            <Apresentation />
         </div>
     );
 }
