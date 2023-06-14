aec0702e46f95f3d9e40fa5c2f02653345639bcc
Your Name <you@example.com>
2023-06-14 18:43:57 -0300

    finishing banner

--- src/components/apresentation/index.js
+++ src/components/apresentation/index.js
@@ -1,13 +1,20 @@
 import '../../styles/Apresentation.css';
+import profile from '../../images/profile.png';
+import line from '../../images/line.svg';
 
 export default function Apresentation() {
     return (
-        <div className="Apresentation">
-            <div className="Container">
-                <p>
+        <div className="Container">
+            <div className="Apresentation">
+                <div>
+                    <img src={profile} />
+                    <p>Olá, eu sou Lyzzandro!</p>
+                </div>
+                <img src={line} />
+                <h1>
                     Crio interfaces para produtos digitais que conectam pessoas
                     e empresas
-                </p>
+                </h1>
             </div>
         </div>
     );
