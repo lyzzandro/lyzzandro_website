33589bf6fe45716d6e0cf74b7866e3221b46839a
Lyzzandro <lyzzandro91@gmail.com>
2023-06-15 10:47:00 -0300

    add favicon, add new animations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu';
 import Apresentation from './components/Apresentation';
 import Footer from './components/Footer';
+import Habilites from './components/Habilities';
 
 function App() {
     return (
         <div className="App">
             <Menu />
             <Apresentation />
+            <Habilites />
             <Footer />
         </div>
     );
