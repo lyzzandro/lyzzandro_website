3ff1b889d51a16639089ede1d2165b6871cb3a7c
Lyzzandro <lyzzandro91@gmail.com>
2023-06-15 12:28:42 -0300

    ajustando width

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,28 +3,30 @@ import logo from '../../logo.svg';
 import arrow from '../../images/chevron-down.svg';
 export default function Menu() {
     return (
-        <header className="Menu">
-            <div className="Container">
-                <img src={logo} alt="Logo" />
-                <ul>
-                    <li>
-                        <a>Sobre mim</a>
-                    </li>
-                    <li>
-                        <a>Galeria</a>
-                    </li>
-                    <li>
-                        <a>Projetos</a>
-                    </li>
-                    <li>
-                        <a>Habilidades</a>
-                    </li>
-                </ul>
-                <button>
-                    Solicitar proposta
-                    <img src={arrow} alt="Arrow" />
-                </button>
-            </div>
-        </header>
+        <div className="Container">
+            <header className="Menu">
+                <div>
+                    <img src={logo} alt="Logo" />
+                    <ul>
+                        <li>
+                            <a>Sobre mim</a>
+                        </li>
+                        <li>
+                            <a>Galeria</a>
+                        </li>
+                        <li>
+                            <a>Projetos</a>
+                        </li>
+                        <li>
+                            <a>Habilidades</a>
+                        </li>
+                    </ul>
+                    <button>
+                        Solicitar proposta
+                        <img src={arrow} alt="Arrow" />
+                    </button>
+                </div>
+            </header>
+        </div>
     );
 }
