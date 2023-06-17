2b0366b2c869e74539b41ef4c1fe294cad08fd9d
Your Name <you@example.com>
2023-06-17 11:57:16 -0300

    add infinite carousel whit logos

--- src/components/Habilities/index.js
+++ src/components/Habilities/index.js
@@ -3,6 +3,15 @@ import mobile from '../../images/smartphone.svg';
 import desktop from '../../images/monitor.svg';
 import code from '../../images/code.svg';
 import arrow from '../../images/arrow-down.svg';
+import { motion } from 'framer-motion';
+import tool1 from '../../images/tool1.svg';
+import tool2 from '../../images/tool2.svg';
+import tool3 from '../../images/tool3.svg';
+import tool4 from '../../images/tool4.svg';
+import tool5 from '../../images/tool5.svg';
+import tool6 from '../../images/tool6.svg';
+import tool7 from '../../images/tool7.svg';
+import tool8 from '../../images/tool8.svg';
 
 export default function Habilites() {
     return (
@@ -46,6 +55,50 @@ export default function Habilites() {
                                 forte apelo visual.
                             </p>
                         </div>
+                    </div>
+                </div>
+                <div className="Logos">
+                    <div class="Logos-slide">
+                        <img src={tool1} />
+                        <img src={tool2} />
+                        <img src={tool3} />
+                        <img src={tool4} />
+                        <img src={tool5} />
+                        <img src={tool6} />
+                        <img src={tool7} />
+                        <img src={tool8} />
+                    </div>
+
+                    <div class="Logos-slide">
+                        <img src={tool1} />
+                        <img src={tool2} />
+                        <img src={tool3} />
+                        <img src={tool4} />
+                        <img src={tool5} />
+                        <img src={tool6} />
+                        <img src={tool7} />
+                        <img src={tool8} />
+                    </div>
+
+                    <div class="Logos-slide">
+                        <img src={tool1} />
+                        <img src={tool2} />
+                        <img src={tool3} />
+                        <img src={tool4} />
+                        <img src={tool5} />
+                        <img src={tool6} />
+                        <img src={tool7} />
+                        <img src={tool8} />
+                    </div>
+                    <div class="Logos-slide">
+                        <img src={tool1} />
+                        <img src={tool2} />
+                        <img src={tool3} />
+                        <img src={tool4} />
+                        <img src={tool5} />
+                        <img src={tool6} />
+                        <img src={tool7} />
+                        <img src={tool8} />
                     </div>
                 </div>
             </div>
