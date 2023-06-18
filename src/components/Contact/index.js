34748882aab9646667093f55ccd7e942ae745cf1
Lyzzandro <lyzzandro91@gmail.com>
2023-06-18 09:31:51 -0300

    add contact area

---
+++ src/components/Contact/index.js
@@ -0,0 +1,54 @@
+import '../../styles/contact.css';
+import user from '../../images/user-red.svg';
+import email from '../../images/email-red.svg';
+import message from '../../images/message-red.svg';
+export default function Contact() {
+    return (
+        <div className="Container">
+            <div className="Contact">
+                <div className="Title">
+                    <p>Entre em contato comigo</p>
+                    <h2>Enviar mensagem</h2>
+                </div>
+                <form>
+                    <div>
+                        <label for="name">
+                            <img src={user} />
+                            Nome
+                        </label>
+                        <input
+                            id="name"
+                            type="text"
+                            placeholder="Digite seu nome completo..."
+                        ></input>
+                    </div>
+
+                    <div>
+                        <label for="email">
+                            <img src={email} />
+                            E-mail
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Digite seu e-mail..."
+                        ></input>
+                    </div>
+
+                    <div>
+                        <label for="message">
+                            <img src={message} />
+                            Mensagem
+                        </label>
+                        <textarea
+                            id="message"
+                            placeholder="Digite sua mensagem..."
+                        ></textarea>
+                    </div>
+
+                    <button type="submit">Enviar mensagem</button>
+                </form>
+            </div>
+        </div>
+    );
+}
