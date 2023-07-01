a5afc6e1d22817f0dacc87d317be5672153392a2
Lyzzandro <lyzzandro91@gmail.com>
2023-07-01 18:16:07 -0300

    add button

--- src/components/Apresentation/index.js
+++ src/components/Apresentation/index.js
@@ -1,6 +1,7 @@
 import '../../styles/apresentation.css';
 import profile from '../../images/profile.png';
 import line from '../../images/line.svg';
+import arrow from '../../images/chevron-down.svg';
 
 export default function Apresentation() {
     return (
@@ -13,8 +14,13 @@ export default function Apresentation() {
                 <img src={line} alt="Linha para separar" />
                 <h1>
                     Construo experiências digitais que unem pessoas e negócios
-                    através de interfaces impactantes
+                    através
+                    <span> de interfaces impactantes</span>
                 </h1>
+                <button className="GradientButton">
+                    Solicitar proposta
+                    <img src={arrow} alt="Arrow" />
+                </button>
             </div>
         </div>
     );
